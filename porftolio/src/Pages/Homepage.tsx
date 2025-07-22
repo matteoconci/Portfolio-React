77f6c6ddc9723b834e321af1ee0a4ae9962ed79e
Matteo Conci <matteo.conci54@hotmail.it>
2025-07-22 12:43:49 +0200

    completed styling from old format

--- porftolio/src/Pages/Homepage.tsx
+++ porftolio/src/Pages/Homepage.tsx
@@ -1,36 +1,33 @@
+import aboutMePhoto from '../assets/AboutMe-Photo.jpeg';
+import css from '../assets/CSS-icon.png';
+import github from '../assets/GitHub-icon.png';
+import html from '../assets/HTML-icon.png';
+import javascript from '../assets/JavaScript-icon.png';
+import linkedin from '../assets/Linkedin-icon.png';
+import mail from '../assets/mail-icon.png';
+import phone from '../assets/phone-icon.png';
 
 const Homepage = () => {
     return (
-           <body>
-            <header>
-                <div class="name">
-                    <h1>Matteo Conci</h1> <!-- valutare se mettere h1 o cosa -->
-                </div>
-                <nav class="nav-laptop">
-                    <div class="list">
-                        <ul class="nav-list">
-                            <li class="nav-item"><a class="a-nav" href="#about-me" onclick="aboutMeDisplay()">About Me</a></li>
-                            <li class="nav-item"><a class="a-nav" href="#project" onclick="projectDisplay()">Project</a></li>
-                            <li class="nav-item"><a class="a-nav" href="#contact" onclick="contactDisplay()">Contact</a></li>
+           <body className="bg-[#EAF4E0] font-chakra max-w-[1000px] mx-auto my-0 text-[1rem] ">
+            <header className="flex mt-4 mb-8">
+                <h1 className="text-2xl">Matteo Conci</h1>
+                <nav className="mr-0 m-auto text-[1.2rem] font-bold">
+                    <div>
+                        <ul className="flex list-none">
+                            <li className="ml-12 hover:underline"><a className="text-black no-underline" href="#about-me">About Me</a></li>
+                            <li className="ml-12 hover:underline"><a className="text-black no-underline" href="#project">Project</a></li>
+                            <li className="ml-12 hover:underline"><a className="text-black no-underline" href="#contact">Contact</a></li>
                         </ul>
                     </div>
                 </nav>
             </header>
-            <nav class="nav-mobile">
-                <div class="list">
-                    <ul class="nav-list">
-                        <li class="nav-item"><a class="a-nav" href="#about-me" onclick="aboutMeDisplay()">About Me</a></li>
-                        <li class="nav-item"><a class="a-nav" href="#project" onclick="projectDisplay()">Project</a></li>
-                        <li class="nav-item"><a class="a-nav" href="#contact" onclick="contactDisplay()">Contact</a></li>
-                    </ul>
-                </div>
-            </nav>
             <main>
-                <section class="about-me" id="about-me">
-                    <article class="about-me-article">
-                        <h2 class="about-me-h2">About Me</h2>
-                        <div class="about-me-content">
-                            <div class="about-me-text">
+                <section id="about-me">
+                    <article>
+                        <h2 className="text-[2rem] mt-8 mb-0">About Me</h2>
+                        <div className="flex text-xl items-center">
+                            <div>
                                 <p>
                                 Hi! I'm Matteo, a junior web developer from Trentino Alto-Adige, Italy. I started my journey in Civil Engeegnering, but after learning some basic Python at the university
                                 I soon realised that this was my correct path, so after leaving my civil career, I started learning coding from the basic with HTML, CSS and Javascript. I'm looking forward to implement
@@ -41,31 +38,31 @@ const Homepage = () => {
                                 Looking forward to learning new things and improving my skills and meet new people to focus on new projects and with the same passion!
                                 </p>
                             </div>
-                            <img class="profile-pic" src="Style/Resorce/Img/AboutMe-Photo.jpeg" alt="Matteo Conci">
+                            <img className="max-w-72 ml-8" src={aboutMePhoto} alt="Matteo Conci" />
                         </div>
                     </article>
-                    <article class="about-me-skills">
-                        <h2>Skills</h2>
-                        <div class="skills">
-                            <ul class="skills-list">
-                                <li class="skill-item">
-                                    <img class="skill-icon" src="Style/Resorce/Img/HTML-icon.png" alt="HTML">
+                    <article className="mt-8">
+                        <h2 className="text-[2rem] mt-8 mb-12">Skills</h2>
+                        <div>
+                            <ul className="flex list-none justify-around mt-8 mr-12">
+                                <li className="text-xl italic font-medium flex items-center">
+                                    <img className="max-w-12 mr-4" src={html} alt="HTML" />
                                     <span>HTML</span>
                                 </li>
-                                <li class="skill-item">
-                                    <img class="skill-icon" src="Style/Resorce/Img/CSS-icon.png" alt="CSS">
+                                <li className="text-xl italic font-medium flex items-center">
+                                    <img className="max-w-12 mr-4" src={css} alt="CSS" />
                                     <span>CSS</span>
                                 </li>
-                                <li class="skill-item">
-                                    <img class="skill-icon" src="Style/Resorce/Img/JavaScript-icon.png" alt="JavaScript">
+                                <li className="text-xl italic font-medium flex items-center">
+                                    <img className="max-w-12 mr-4" src={javascript} alt="JavaScript" />
                                     <span>JavaScript</span>
                                 </li>
-                                <li class="skill-item">
-                                    <img class="skill-icon" src="Style/Resorce/Img/Python-icon.png" alt="Python">
+                                {/* <li className="text-xl italic font-medium flex items-center">
+                                    <img className="max-w-12 mr-4" src="Style/Resorce/Img/Python-icon.png" alt="Python" />
                                     <span>Python</span>
-                                </li>
-                                <li class="skill-item">
-                                    <img class="skill-icon" src="Style/Resorce/Img/GitHub-icon.png" alt="GitHub">
+                                </li> */}
+                                <li className="text-xl italic font-medium flex items-center">
+                                    <img className="max-w-12 mr-4" src={github} alt="GitHub" />
                                     <span>GitHub</span>
                                 </li>
                             </ul>
@@ -73,72 +70,72 @@ const Homepage = () => {
                     </article>
                 </section>
 
-                <section class="project" id="project">
-                        <h2>Projects</h2>
-                        <div class="projects-navbar">
-                            <ul class="projects-list">
-                                <li class="project-flexbox" onclick="flexboxDisplay()">Flexbox Business site</li>
-                                <li class="projects-space">|</li>
-                                <li class="project-2" onclick="twoDisplay()">Project 2</li>
-                                <li class="projects-space">|</li>
-                                <li class="project-3" onclick="threeDisplay()">Project 3</li>
+                <section id="project">
+                        <h2 className="text-[2rem] mt-8 mb-12">Projects</h2>
+                        <div>
+                            <ul className="flex list-none justify-between text-2xl font-medium">
+                                <li className="italic no-underline hover:cursor-pointer hover:underline">Flexbox Business site</li>
+                                <li>|</li>
+                                <li className="italic no-underline hover:cursor-pointer hover:underline">Project 2</li>
+                                <li>|</li>
+                                <li className="italic no-underline hover:cursor-pointer hover:underline">Project 3</li>
                             </ul>
                         </div>
-                        <div class="projects-content">
-                            <div class="flexbox" id="flexbox">
-                                <div class="link-flexbox">
-                                    <a href="https://matteoconci.github.io/flexbox-business-site-starting/" target="_blank">Link to the website</a>
+                        <div className="text-xl mt-10">
+                            <div id="flexbox">
+                                <div>
+                                    <a href="https://matteoconci.github.io/flexbox-business-site-starting/" target="_blank" className="text-black hover:no-underline">Link to the website</a>
                                 </div>
-                                    <p class="text-flexbox">
-                                        An exercise using flexbox with the goal to reproduce a business site for a company. <br>
+                                    <p>
+                                        An exercise using flexbox with the goal to reproduce a business site for a company. <br></br>
                                         Language used: HTML and CSS.
                                     </p>
                             </div>
-                            <div class="two" id="two">
-                                <div class="link-project2">
-                                    <a href="#" target="_blank">Link to the website</a>
+                            <div id="two">
+                                <div>
+                                    <a href="#" target="_blank" className="text-black hover:no-underline">Link to the website</a>
                                 </div>
-                                    <p class="text-project2">
-                                        An exercise using. <br>
+                                    <p>
+                                        An exercise using. <br></br>
                                         Language used: .
                                     </p>
                             </div>
-                            <div class="three" id="three">
-                                <div  class="link-project3">
-                                    <a href="#" target="_blank">Link to the website</a>
+                            <div id="three">
+                                <div>
+                                    <a href="#" target="_blank" className="text-black hover:no-underline">Link to the website</a>
                                 </div>
-                                    <p class="text-project3">
-                                        An exercise using . <br>
+                                    <p>
+                                        An exercise using . <br></br>
                                         Language used: .
                                     </p>
                             </div>
                         </div>
                 </section>
 
-                <section class="contact" id="contact">
-                    <h2>Contacts</h2>
-                    <p class="contact-text">
+                <section className="mb-20" id="contact">
+                    <h2 className="text-[2rem] mt-8 mb-12">Contacts</h2>
+                    <p className="text-xl">
                         If you want to reach me out for future projects feel free to contact me.
                     </p>
-                    <div class="contact-icon">
-                        <div class="mail-contact">
+                    <div className="flex items-center justify-evenly mt-12">
+                        <div>
                             <a href="mailto:matteo.conci54@hotmail.it?subject=Request%20for%20Information&body=Hello,%20I%20would%20like%20more%20information.">
-                                <img class="img-icon" src="Style/Resorce/Img/mail-icon.png" alt="Send an email">
+                                <img className="max-w-[3.5rem]" src={mail} alt="Send an email" />
                             </a>
                         </div>
-                        <div class="phone-contact">
+                        <div>
                             <a href="tel:+393451790418" target="_blank">
-                                <img class="img-icon" src="Style/Resorce/Img/phone-icon.png" alt="Phone contact">
+                                <img className="max-w-[2.1rem]" src={phone} alt="Phone contact" />
                             </a>
                         </div>
-                        <div class="github-contact">
+                        <div>
                             <a href="https://github.com/matteoconci" target="_blank">
-                                <img class="img-icon" src="Style/Resorce/Img/GitHub-icon.png" alt="Github contact">
+                                <img className="max-w-12" src={github} alt="Github contact" />
                             </a>
                         </div>
-                        <div class="linkedin-contact">
+                        <div>
                             <a href="#" target="_blank">
-                            <img class="img-icon" src="Style/Resorce/Img/Linkedin-icon.png" alt="Linkedin contact">
+                            <img className="max-w-12" src={linkedin} alt="Linkedin contact" />
                         </a>
                         </div>
                     </div>
---
+++ porftolio/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        chakra: ["'Chakra Petch'", "serif"],
+      },
+    },
+  },
+}
