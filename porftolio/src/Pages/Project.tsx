675b64d2a0f9df52c4222c6e068f0751eb1e18a4
Matteo Conci <matteo.conci54@hotmail.it>
2025-07-28 18:15:22 +0200

    Fix project

--- porftolio/src/Pages/Project.tsx
+++ porftolio/src/Pages/Project.tsx
@@ -7,7 +7,7 @@ const Project = () => {
         <div className="bg-[#EAF4E0] font-chakra max-w-[1000px] mx-auto my-0 text-[1rem]">
             <h2 className="text-[2rem] mt-8 mb-12 font-semibold">Projects</h2>
             <div>
-                <ul className="flex list-none justify-evenly text-2xl font-medium">
+                <ul className="flex list-none justify-evenly text-3xl font-medium">
                     <li className={`italic no-underline hover:cursor-pointer hover:underline ${project === 1 ? 'underline text-[#ae884c]' : ''}`} onClick={() => setProject(1)}>Ecommerce App Marketplace Shopp</li>
                     <li>|</li>
                     <li className={`italic no-underline hover:cursor-pointer hover:underline ${project === 2 ? 'underline text-[#ae884c]' : ''}`} onClick={() => setProject(2)}>Reddit Clone</li>
@@ -15,43 +15,45 @@ const Project = () => {
             </div>
             <div className="text-xl mt-10">
                 {project === 1 &&
-                    <div>
+                    <div className="mb-50">
                         <p className="mb-6">Link to the <a href='' target="_blank" className="italic underline hover:no-underline text-[#ae884c]">GitHub repository</a>.</p>
-                        <p className="italic text-[1.1rem]">This project is still in progress.</p>
-                        <p className="mt-4">
-                            Full-stack web application designed to simulate a multi-role e-commerce platform, with different UI and functionality for buyer, seller and admin. <br></br>
-                            In the marketplace the user can create an account as a buyer or can create a store and become a seller.<br></br>
-                        </p>
-                        <p className="mt-4">The main functionality of the project are:</p>
+                        <p className="italic text-[1.1rem]">This project is still in progress. See some picture below.</p>
+                        <p className="mt-10 font-semibold text-xl">Full-stack web application designed to simulate a multi-role e-commerce platform, with dedicated UIs and functionalities for buyers, sellers and admins.</p>
+                        <p className="mt-2 text-lg">Users can sign up as buyers or create a store to act as sellers, while the admin oversees the platform with extended management capabilities.</p>
+                        <p className="mt-2 font-semibold text-xl">Key features include:</p>
                         <ol className="list-disc ml-10 text-lg">
-                            <li>login, registration and logout feature;</li>
-                            <li>Authorization and Authentication for buyer, seller and admin;</li>
-                            <li>CRUD functionality for product;</li>
-                            <li>Dashboard for every role;</li>
-                            <li>Hot product and sales in the homepage;</li>
-                            <li>Cart, checkout and shipment tracking;</li>
+                            <li>User registration, login, and logout;</li>
+                            <li>Role-based authentication and authorization (buyer, seller, admin);</li>
+                            <li>Full CRUD functionality for products and its variants;</li>
+                            <li>Dedicated dashboards for each user role;</li>
+                            <li>Highlighted products and promotions on the homepage;</li>
+                            <li>Shopping cart, checkout flow, and shipment tracking;</li>
+                            <li>Store creation and management for sellers;</li>
+                            <li>Secure backend with middleware and validations;</li>
                         </ol>
-                        <p className="mt-4">The main technology used are:</p>
+                        <p className="mt-4 font-semibold text-xl">Main technologies used:</p>
                         <ol className="list-disc ml-10 text-lg">
-                            <li>JavaScript, Typescript, React, Redux and Tailwind CSS for the Front-End;</li>
-                            <li>Express.js, Node.js, PostgreSQL and Rest-API for the Back-end;</li>
-                            <li>JWT, Bcrypt, CORS, Helmet for Auth and security;</li>
-                            <li>Figma for the UI Design;</li>
-                            <li>Jira, GitHub and Supabase for version control and task manegement;</li>
-                            <li>Responsive desing ;</li>
+                            <li><span className="font-semibold">Frontend</span>: JavaScript, TypeScript, React, Redux Toolkit, Tailwind CSS;</li>
+                            <li><span className="font-semibold">Backend</span>: Node.js, Express.js, PostgreSQL, RESTful API architecture;</li>
+                            <li><span className="font-semibold">Authentication & Security</span>: JWT, Bcrypt, CORS, Helmet, Rate Limiter;</li>
+                            <li><span className="font-semibold">Design</span>: Figma (UI/UX);</li>
+                            <li><span className="font-semibold">Project Management & Deployment</span>: GitHub, Jira, Supabase;</li>
+                            <li><span className="font-semibold">Additional</span>: Fully responsive design and modular architecture;</li>
                         </ol>
+                        <p className="mt-4 font-semibold text-xl">Pages:</p>
+                        <img></img>
                     </div>
                 }
                 {project === 2 &&  
-                <div>
-                    <div>
-                        <a href="#" target="_blank" className="hover:no-underline">Link to the website</a>
+                    <div className="mb-50">
+                        <div>
+                            <a href="#" target="_blank" className="hover:no-underline">Link to the website</a>
+                        </div>
+                            <p>
+                                An exercise using. <br></br>
+                                Language used: .
+                            </p>
                     </div>
-                        <p>
-                            An exercise using. <br></br>
-                            Language used: .
-                        </p>
-                </div>
                 }
             </div>
         </div>
