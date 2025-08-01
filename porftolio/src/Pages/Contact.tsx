d397eb74cffef477e4ebd0ccd69345ccdf65924f
Matteo Conci <matteo.conci54@hotmail.it>
2025-08-01 18:05:04 +0200

    Width contact image

--- porftolio/src/Pages/Contact.tsx
+++ porftolio/src/Pages/Contact.tsx
@@ -14,21 +14,30 @@ const Contact = () => {
             <div className="flex items-center justify-center mt-12">
                 <div className='group mt-8 mr-2'>
                     <a href="mailto:matteo.conci54@hotmail.it?subject=Request%20for%20Information&body=Hello,%20I%20would%20like%20more%20information.">
-                        <img className="max-w-[3.8rem] m-auto" src={mail} alt="Send an email" />
+                        <img className="max-w-[3.8rem] m-auto"
+                            width={60} 
+                            src={mail} 
+                            alt="Send an email" />
                     </a>
                     <div className='text-center mt-2 opacity-0 group-hover:opacity-100'>
                         matteo.conci54@hotmail.it
                     </div>
                 </div>
                 <div className='mt-10 group mr-9'>
-                    <img className='max-w-8 m-auto' src={phone} alt="Phone contact" />
+                    <img className='max-w-8 m-auto'
+                        width={60} 
+                        src={phone} 
+                        alt="Phone contact" />
                     <div className='text-center mt-4 opacity-0 group-hover:opacity-100'>
                         +39 345 1790418
                     </div>
                 </div>
                 <div className='mt-10 group mr-12'>
                     <a href="https://wa.me/393451790418" target="_blank" rel="noopener noreferrer">
-                        <img className='max-w-12 m-auto' src={whatsapp} alt="WhatsApp contact" />
+                        <img className='max-w-12 m-auto'
+                            width={60}  
+                            src={whatsapp} 
+                            alt="WhatsApp contact" />
                     </a>
                     <div className='text-center mt-4 opacity-0 group-hover:opacity-100'>
                         +39 345 1790418
@@ -36,7 +45,10 @@ const Contact = () => {
                 </div>
                 <div className='group mt-10 mr-16'>
                     <a href="https://github.com/matteoconci" target="_blank">
-                        <img className="max-w-12 m-auto" src={github} alt="Github contact" />
+                        <img className="max-w-12 m-auto"
+                            width={60}  
+                            src={github} 
+                            alt="Github contact" />
                     </a>
                     <div className='text-center mt-4 opacity-0 group-hover:opacity-100'>
                         <a href="https://github.com/matteoconci" className='italic hover:underline' target="_blank">Matteo Conci</a>
@@ -44,7 +56,10 @@ const Contact = () => {
                 </div>
                 <div className='group mt-10 mr-15'>
                     <a href="#" target="_blank">
-                        <img className="max-w-12 m-auto" src={linkedin} alt="Linkedin contact" />
+                        <img className="max-w-12 m-auto"
+                            width={60}  
+                            src={linkedin} 
+                            alt="Linkedin contact" />
                     </a>
                     <div className='text-center mt-4 opacity-0 group-hover:opacity-100'>
                         <a href="" className='italic hover:underline' target="_blank">Matteo Conci</a>
