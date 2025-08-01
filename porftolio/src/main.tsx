7a171ef073a83ba30f9f33111ddeb2c46382a343
Matteo Conci <matteo.conci54@hotmail.it>
2025-08-01 17:27:14 +0200

    Add analytics for vercel

--- porftolio/src/main.tsx
+++ porftolio/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 );
