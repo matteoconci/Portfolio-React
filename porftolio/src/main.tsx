ad62752dbcdc02f9fc4678d30b4cce099e7e781c
Matteo Conci <matteo.conci54@hotmail.it>
2025-08-01 17:35:18 +0200

    Remove analytics

--- porftolio/src/main.tsx
+++ porftolio/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from "@vercel/analytics/next"
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <SpeedInsights />
-    <Analytics />
   </StrictMode>,
 );
